7f2c2bb64d1b15c159abb73a882a9613ab871e0a
Vivien Adnot <vadnot@kabbyplus.com>
2025-07-07 19:52:08 +0200

    wild-rydes

---
+++ aws/amplify/wild-rydes/src/config.js
@@ -0,0 +1,7 @@
+// Add your API invoke URL below, configured in Module 3. 
+
+module.exports = {
+    api: {
+        invokeUrl: 'https://r3lzgccse6.execute-api.eu-west-3.amazonaws.com/prod'
+    }
+}
